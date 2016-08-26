1daaba776df8eb7c2ef20cbb7bc8d0e57419365f
Till-Markus Hülsemann <till@disroot.org>
2016-08-26 19:57:27 +0200

    modules/packageloader/class_packageloader - rename $name -> $key

--- modules/packageLoader/class_Package.php
+++ modules/packageLoader/class_Package.php
@@ -1,68 +1,69 @@
-<?php 
+<?php
 class Package {
 
 	/**
 	 * @param $type "module" or "plugin"
-	 * @param $name the name of the package
+	 * @param $key the key the package is registered under
 	 * @param $category "classes" or "functions"
 	 */
-	private static function loadNonExecutable($type, $name, $category) {
+	private static function loadNonExecutable($type, $key, $category) {
 		global $config;
-		if (!isset($config[$type][$name][$category])) return;
-		foreach ($config[$type][$name][$category] as $file) {
-			include_once $config[$type][$name]['path'] . $file;
+		if (!isset($config[$type][$key][$category])) return;
+		foreach ($config[$type][$key][$category] as $file) {
+			include_once $config[$type][$key]['path'] . $file;
 		}
 	}
 
 	/**
 	 * @param $type "module" or "plugin"
-	 * @param $name the name of the package
+	 * @param $key the key the package is registered under
 	 */
-	private static function loadClassesAndFunctions($type, $name) {
-		Package::loadNonExecutable($type, $name, 'classes');
-		Package::loadNonExecutable($type, $name, 'functions');
+	private static function loadClassesAndFunctions($type, $key) {
+		Package::loadNonExecutable($type, $key, 'classes');
+		Package::loadNonExecutable($type, $key, 'functions');
 	}
 
-	private static function loadExecutables ($type, $name) {
+	private static function loadExecutables ($type, $key) {
 		global $config;
-		if (!isset($config[$type][$name])) return;
-		if (isset($config[$type][$name]['execute'])) {
-			ksort($config[$type][$name]['execute']);
-			foreach ($config[$type][$name]['execute'] as $file) {
-				registerExecutable($config[$type][$name]['path'] . $file);
+		if (!isset($config[$type][$key])) return;
+		if (isset($config[$type][$key]['execute'])) {
+			ksort($config[$type][$key]['execute']);
+			foreach ($config[$type][$key]['execute'] as $file) {
+				registerExecutable($config[$type][$key]['path'] . $file);
 			}
 		}
 	}
 
-	static function load ($type, $name) {
+	static function load ($type, $key) {
 		global $config;
-		if (!isset($config[$type][$name])) return;
-		if (isset ($config[$type][$name]['isLoaded']) && 
-				($config[$type][$name]['isLoaded'] === true)) return;
-				
-		$path = $config[$type][$name];
+		if (!isset($config[$type][$key])) return;
+		if (isset ($config[$type][$key]['isLoaded']) &&
+				($config[$type][$key]['isLoaded'] === true)) return;
+
+		$path = $config[$type][$key];
 		if (substr($packagePath, -1) !== "/") {
 			$packagePath .= '/';
 		}
 
-		$config[$type][$name] = parse_ini_file($path . 'config.ini', true);
-		$config[$type][$name]['path'] = $path;
+		$config[$type][$key] = parse_ini_file($path . 'config.ini', true);
+		loadLocalConfig(); 
+		$config[$type][$key]['path'] = $path;
 
-		Package::loadClassesAndFunctions($type, $name);
-		Package::loadExecutables($type, $name);
-		$config[$type][$name]['isLoaded'] = true;
+		Package::loadClassesAndFunctions($type, $key);
+		Package::loadExecutables($type, $key);
+		$config[$type][$key]['isLoaded'] = true;
 	}
 
 	static function loadAll () {
 		global $config;
 		if (isset($config['module'])) {
-			foreach ($config['module'] as $name => $tmp) {
-				Package::load('module', $name);
+			foreach ($config['module'] as $key => $tmp) {
+				Package::load('module', $key);
 			}
 		}
 		if (isset($config['plugin'])) {
-			foreach ($config['plugin'] as $name => $tmp) {
-				Package::load('plugin', $name);
+			foreach ($config['plugin'] as $key => $tmp) {
+				Package::load('plugin', $key);
 			}
 		}
 	}
