0fa165eef2067385affa86a4171d7603c7cca147
Till-Markus Hülsemann <till@disroot.org>
2016-10-02 10:46:30 +0200

    load functions.php before init.php
    init.php uses global functions and therefore needs functions.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-include_once 'core/init.php';
 include_once 'core/functions.php';
+include_once 'core/init.php';
 include_once $config['module']['packageLoader'] . 'bootstrap.php';
 Package::loadAll();
 
